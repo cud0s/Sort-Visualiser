2aecbb92ae489b990ebc939e6ade2c9e566091c3
Cud0s <sl.vaidas@gmail.com>
2015-05-20 19:01:38 +0300

    Initial commit

---
+++ SortVisualiser/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortVisualiser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sortButton_Click(object sender, EventArgs e)
+        {
+            Object selectedLoad = listBox.SelectedItem;
+            if (selectedLoad is Sort)
+            {
+                ((Sort)selectedLoad).startSorting(richTextBox1.Text);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.ChartAreas["ChartArea1"].AxisX.LabelStyle.Enabled = false; 
+            chart1.ChartAreas["ChartArea1"].AxisX.MajorGrid.Enabled = false;
+            chart1.ChartAreas["ChartArea1"].AxisY.MajorGrid.Enabled = false;
+            
+            chart1.Series["series1"].Points.AddY(9);
+            chart1.Series["series1"].Points[0].Color = Color.Transparent;
+            chart1.BackColor = chart1.ChartAreas["ChartArea1"].BackColor = Color.Transparent;
+
+            listBox.Items.Add(new BubbleSort(richTextBox2, richTextBox4, richTextBox3, richTextBox5, richTextBox6, richTextBox7, chart1, checkBox1.Checked));
+            listBox.Items.Add(new InsertionSort(richTextBox2, richTextBox4, richTextBox3, richTextBox5, richTextBox6, richTextBox7, chart1, checkBox1.Checked));
+            listBox.Items.Add(new SelectionSort(richTextBox2, richTextBox4, richTextBox3, richTextBox5, richTextBox6, richTextBox7, chart1, checkBox1.Checked));
+            listBox.Items.Add(new QuickSort(richTextBox2, richTextBox4, richTextBox3, richTextBox5, richTextBox6, richTextBox7, chart1, checkBox1.Checked));
+
+
+            ToolTip toolTip1 = new ToolTip();
+            toolTip1.ShowAlways = true;
+            toolTip1.SetToolTip(label8, "SWAG - scientifically based wild ass guess");
+            toolTip1.AutoPopDelay = 3000;
+            toolTip1.InitialDelay = 600;
+            toolTip1.ReshowDelay = 1000;
+
+            comboBox1.SelectedIndex = 1;
+            listBox.SelectedIndex = 0;
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            double timeInS = Double.Parse(comboBox1.SelectedItem.ToString());
+            int timeInMs = Convert.ToInt32(timeInS * 1000);
+            Sort.sleepTime = timeInMs;
+        }
+
+    }
+}
---
+++ SortVisualiser/QuickSort.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SortVisualiser
+{
+    class QuickSort : Sort
+    {
+
+        public override void startSorting(string newInputNumbers)
+        {
+            inputData(newInputNumbers);
+            reorder(0, numbers.Count - 1);
+        }
+
+        private void reorder(int from, int to)
+        {
+            int pivot = choosePivot(from, to);
+
+            usedMemory++;
+            swapAndMark(pivot, to);
+            int storeIndex = from;
+            for (int a = from; a < to; a++)
+            {
+                logComparison(a, to);
+                if (numbers[a] <= numbers[to])
+                {
+                    swapAndMark(storeIndex, a);
+                    storeIndex++;
+                }
+            }
+            swapAndMark(storeIndex, to);
+            sortedList.Add(storeIndex);
+
+            logComparison(from, storeIndex - 1);
+            if (from < storeIndex - 1)
+            {
+                reorder(from, storeIndex - 1);
+            }
+            else
+            {
+                sortedList.Add(from);
+                sortedList.Add(storeIndex - 1);
+            }
+
+            logComparison(to, storeIndex + 1);
+            if (storeIndex + 1 < to)
+            {
+                reorder(storeIndex + 1, to);
+            }
+            else
+            {
+                sortedList.Add(to);
+                sortedList.Add(storeIndex + 1);
+            }
+            usedMemory--;
+        }
+
+        private int partition(int from, int to)
+        {
+            return choosePivot(from, to);
+        }
+
+        private int choosePivot(int from, int to)
+        {
+            Debug.WriteLine((to + from) / 2);
+            return (to + from) / 2;
+        }
+
+        public override string ToString()
+        {
+            return "Quick Sort";
+        }
+        public QuickSort(RichTextBox a, RichTextBox b, RichTextBox c, RichTextBox d, RichTextBox e, RichTextBox f, Chart g, bool h)
+            : base(a, b, c, d, e, f, g, h)
+        {
+        }
+    }
+}
